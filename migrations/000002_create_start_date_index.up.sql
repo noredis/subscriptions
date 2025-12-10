a084a0e9799bbd63b1dc63d4782f04886b6c53db
Rustam Urazov <noredis@yandex.ru>
2025-12-10 16:12:16 +0600

    start_date, end_date indexes

---
+++ migrations/000002_create_start_date_index.up.sql
@@ -0,0 +1,1 @@
+CREATE INDEX IF NOT EXISTS idx_subscriptions_start_date ON subscriptions(start_date);
---
+++ migrations/000003_create_end_date_index.up.sql
@@ -0,0 +1,1 @@
+CREATE INDEX IF NOT EXISTS idx_subscriptions_end_date ON subscriptions(end_date);
